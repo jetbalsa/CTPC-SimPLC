a1b7afabd7c73730ba30b2668469d3e7cb1b739b
Wasabi Burns <69662763+spiceywasabi@users.noreply.github.com>
2020-08-29 20:03:08 -0700

    fix missing quote

--- launch.sh
+++ launch.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-if [ ! -z "$CONTAINER_SYSLOGDEST ]; then
+if [ ! -z "$CONTAINER_SYSLOGDEST" ]; then
 	syslogd -R "$CONTAINER_SYSLOGDEST" -D
 fi
 
 
 while true; do socat tcp-l:502,reuseaddr,fork exec:./lake-level.php; sleep 1; done
 
-cd /simulator 
+cd /simulator
 
 if [ ! -z "$CONTAINER_SIMULATION" ]; then
 	# make it accessible for everything
@@ -16,11 +16,11 @@ if [ ! -z "$CONTAINER_SIMULATION" ]; then
 	if [ ! -f "$LAUNCHER" ]; then
 		echo "error invalid simulation type"
 	fi
-	
+
 	while true; do
 		if [ -f "/stop" ]; then
 			exit 0
-		else 
+		else
 			socat "tcp-l:5502,reuseaddr,fork exec:./$LAUNCHER"
 			sleep 2
 		fi
